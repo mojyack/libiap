09a116ba7719d43aa97b122ce7c77121a841374e
mojyack <mojyack@gmail.com>
2025-11-24 22:07:05 +0900

    iap: hid: use correct report id

--- src/iap/hid.c
+++ src/iap/hid.c
@@ -6,44 +6,45 @@
 #include "platform.h"
 #include "spec/hid.h"
 
-/* must match to in-driver hid descriptor */
 struct ReportSize {
     uint8_t id;
     uint8_t size; /* including link control byte */
 };
-/* report_id -> size map */
-static struct ReportSize report_size_table[] = {
-    {0x01, 0x0C},
-    {0x02, 0x0E},
-    {0x03, 0x14},
-    {0x04, 0x3F},
-    {0x05, 0x08},
-    {0x06, 0x0A},
-    {0x07, 0x0E},
-    {0x08, 0x14},
-    {0x09, 0x3F},
+
+/* must match to in-driver hid report descriptor */
+
+static struct ReportSize output_report_size_table[] = {
+    {.id = 0x05, .size = 0x08},
+    {.id = 0x06, .size = 0x0A},
+    {.id = 0x07, .size = 0x0E},
+    {.id = 0x08, .size = 0x14},
+    {.id = 0x09, .size = 0x3F},
 };
 
 /* sorted by size */
-static struct ReportSize report_id_table[] = {
-    /*{0x05, 0x08},*/
-    /*{0x06, 0x0A},*/ /* why this is rejected? */
-    {0x01, 0x0C},
-    {0x02, 0x0E},
-    {0x07, 0x0E},
-    {0x03, 0x14},
-    {0x08, 0x14},
-    {0x04, 0x3F},
-    {0x09, 0x3F},
+static struct ReportSize input_report_size_table[] = {
+    {.id = 0x01, .size = 0x0C},
+    {.id = 0x02, .size = 0x0E},
+    {.id = 0x03, .size = 0x14},
+    {.id = 0x04, .size = 0x3F},
 };
+
+static int find_output_report_size(uint8_t id) {
+    for(size_t i = 0; i < array_size(input_report_size_table); i += 1) {
+        if(output_report_size_table[i].id == id) {
+            return output_report_size_table[i].size;
+        }
+    }
+    return -1;
+}
 
 IAPBool iap_feed_hid_report(struct IAPContext* ctx, const uint8_t* const data, const size_t size) {
     check_ret(size > sizeof(struct IAPHIDReport), iap_false);
-    struct IAPHIDReport* report      = (struct IAPHIDReport*)data;
-    const uint8_t        table_index = report->report_id - 1;
-    check_ret(table_index < array_size(report_size_table), iap_false);
-    const uint8_t report_size = report_size_table[table_index].size;
-    check_ret(report_size == size - 1, iap_false); /* TODO: shoud we check this? */
+    struct IAPHIDReport* report = (struct IAPHIDReport*)data;
+
+    const int report_size = find_output_report_size(report->report_id);
+    check_ret(report_size == size - 1, iap_false);
+
     const uint8_t payload_size = report_size - 1;
     check_act(ctx->hid_recv_buf_cursor + payload_size <= HID_BUFFER_SIZE, { ctx->hid_recv_buf_cursor = 0; return iap_false; }, "hid buffer overflow");
     if(!(report->link_control & IAPHIDReportLinkControlBits_Continue)) {
@@ -87,12 +88,12 @@ IAPBool _iap_send_hid_reports(struct IAPContext* ctx, size_t begin, size_t end) 
 }
 
 static struct ReportSize* find_optimal_report_size(size_t size) {
-    for(size_t i = 0; i < array_size(report_id_table); i += 1) {
-        if(report_id_table[i].size >= size + 1 /* link control byte*/) {
-            return &report_id_table[i];
+    for(size_t i = 0; i < array_size(input_report_size_table); i += 1) {
+        if(input_report_size_table[i].size >= size + 1 /* link control byte*/) {
+            return &input_report_size_table[i];
         }
     }
-    return &report_id_table[array_size(report_id_table) - 1];
+    return &input_report_size_table[array_size(input_report_size_table) - 1];
 }
 
 IAPBool _iap_send_next_report(struct IAPContext* ctx) {
