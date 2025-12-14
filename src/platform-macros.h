44636f3a679ac14eab06802d1655bce9b1e24946
mojyack <mojyack@gmail.com>
2025-12-15 01:40:55 +0900

    remove unused IAP_WARNF

--- src/platform-macros.h
+++ src/platform-macros.h
@@ -6,12 +6,6 @@
         printf(__VA_ARGS__); \
         printf("\n");        \
     }
-#define IAP_WARNF(...)       \
-    {                        \
-        printf("\x1B[91m");  \
-        printf(__VA_ARGS__); \
-        printf("\x1B[0m\n"); \
-    }
 #define IAP_ERRORF(...)      \
     {                        \
         printf("\x1B[93m");  \
