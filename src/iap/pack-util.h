f43032239d0557b193a30bcbf9ba96ad769b0965
mojyack <mojyack@gmail.com>
2025-12-15 21:54:04 +0900

    iap: pack-util: fix warnings

--- src/iap/pack-util.h
+++ src/iap/pack-util.h
@@ -1,9 +1,10 @@
+#include <stdint.h>
 #include <string.h>
 
-#include "iap.h"
+#include "bool.h"
 #include "macros.h"
 
-static IAPBool pack_u8(uint8_t** data, size_t* size, uint8_t value) {
+__attribute__((unused)) static IAPBool pack_u8(uint8_t** data, size_t* size, uint8_t value) {
     check_ret(*size >= 1, iap_false);
     (*data)[0] = value;
     *data += 1;
@@ -11,7 +12,7 @@ static IAPBool pack_u8(uint8_t** data, size_t* size, uint8_t value) {
     return iap_true;
 }
 
-static IAPBool pack_u16(uint8_t** data, size_t* size, uint16_t value) {
+__attribute__((unused)) static IAPBool pack_u16(uint8_t** data, size_t* size, uint16_t value) {
     check_ret(*size >= 2, iap_false);
     (*data)[0] = value >> 8;
     (*data)[1] = value;
@@ -20,7 +21,7 @@ static IAPBool pack_u16(uint8_t** data, size_t* size, uint16_t value) {
     return iap_true;
 }
 
-static IAPBool pack_u32(uint8_t** data, size_t* size, uint32_t value) {
+__attribute__((unused)) static IAPBool pack_u32(uint8_t** data, size_t* size, uint32_t value) {
     check_ret(*size >= 2, iap_false);
     (*data)[0] = value >> 24;
     (*data)[1] = value >> 16;
@@ -31,7 +32,7 @@ static IAPBool pack_u32(uint8_t** data, size_t* size, uint32_t value) {
     return iap_true;
 }
 
-static IAPBool pack_data(uint8_t** data, size_t* size, const void* payload, size_t payload_size) {
+__attribute__((unused)) static IAPBool pack_data(uint8_t** data, size_t* size, const void* payload, size_t payload_size) {
     check_ret(*size >= payload_size, iap_false);
     memcpy(*data, payload, payload_size);
     *data += payload_size;
