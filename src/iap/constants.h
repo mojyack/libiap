474307c0cadcb175c643185780cf71e95085a692
mojyack <mojyack@gmail.com>
2026-03-30 16:32:25 +0900

    iap: optimize SEND_BUFFER_SIZE for artwork transmission

--- src/iap/constants.h
+++ src/iap/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define HID_BUFFER_SIZE  1024
-#define SEND_BUFFER_SIZE 1024
+#define SEND_BUFFER_SIZE 767 /* 0x2ff - 1: input_report_size_table_hs[0x0C].size - sizeof(report_id) */
 
 enum IAPPhase {
     IAPPhase_Connected = 0, /* initial state, waiting for StartIDPS */
